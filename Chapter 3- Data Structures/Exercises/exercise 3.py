5f6811e3074c8ad7363782ea17c09fe892e49424
avacinth <toribiohyacinth05@gmail.com>
2023-10-22 23:28:37 +0400

    Create exercise 3.py

---
+++ Chapter 3- Data Structures/Exercises/exercise 3.py
@@ -0,0 +1,14 @@
+# Exercise 3: Your Own List :ballot_box_with_check:
+
+modes_of_transportation = ["car", "motorcycle", "bicycle", "scooter"]
+
+# Print statements about these items
+for item in modes_of_transportation:
+    if item == "car":
+        print(f"I would like to own a {item}.")
+    if item == "motorcycle":
+        print(f"I've always wanted a {item}.")
+    if item == "bicycle":
+        print(f"Riding a {item} is a great way to stay fit.")
+    if item == "scooter":
+        print(f"A {item} is perfect for city commuting.")
