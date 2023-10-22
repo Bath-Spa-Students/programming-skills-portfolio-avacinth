a37dfa6449065743424f56e8f8f7bf164b1643aa
avacinth <toribiohyacinth05@gmail.com>
2023-10-22 21:47:47 +0400

    Create exercise 2.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise 2.py
@@ -0,0 +1,4 @@
+# Exercise 2: Variables :ballot_box_with_check:
+famous_person = 'Melinda Kunst'
+message = f'{famous_person} once said, "Little by Little, Day by Day. What Is Meant for You Will Find Its Way."'
+print (message)
