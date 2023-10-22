1fa0a9e52cfc957ccd08f027c1dc263185ed65d9
avacinth <toribiohyacinth05@gmail.com>
2023-10-22 22:03:03 +0400

    Create exercise 2.py

---
+++ Chapter 3- Data Structures/Exercises/exercise 2.py
@@ -0,0 +1,13 @@
+# Exercise 2: Greetings :ballot_box_with_check:
+
+names = ['Sofia', 'Shaima', 'Myen']
+
+msg = "How are you today, " + names[0] + "?"
+print(msg)
+
+msg = "How are you today, " + names[1] + "?"
+print(msg)
+
+msg = "How are you today, " + names[2] + "?"
+
+print(msg)
