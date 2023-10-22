2b13d214bd534783626b26576b8d3b99bae59c3b
avacinth <toribiohyacinth05@gmail.com>
2023-10-22 23:28:48 +0400

    Create exercise 4.py

---
+++ Chapter 3- Data Structures/Exercises/exercise 4.py
@@ -0,0 +1,19 @@
+# Exercise 4: Guest List :ballot_box_with_check:
+
+# Inviting people to dinner.
+guests = ['Sofia Borcelo', 'Shaima Francisco', 'Myen Naguit', 'RJ Tobias', 'Erich Lubis']
+
+name = guests[0]
+print(name + ", I would like to invite you to dinner.")
+
+name = guests[1]
+print(name + ", I would like to invite you to dinner.")
+
+name = guests[2]
+print(name + ", I would like to invite you to dinner.")
+
+name = guests[3]
+print(name + ", I would like to invite you to dinner.")
+
+name = guests[4]
+print(name + ", I would like to invite you to dinner.")
