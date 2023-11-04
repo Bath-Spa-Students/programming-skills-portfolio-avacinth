be2a906fe11c2f7cd9ed3969a1b2840f164cee65
avacinth <144213493+avacinth@users.noreply.github.com>
2023-11-05 00:10:48 +0400

    practice 4.py

--- Chapter 1- Getting Started with Python/Practices/practice 4.py
+++ Chapter 1- Getting Started with Python/Practices/practice 4.py
@@ -1,13 +1,14 @@
-# Compute the area of a triangle 
+# Computing the area of a triangle
 
-# Three sides of the triangle is a, b and c:  
-a = float(input(10))  
-b = float(input(12))  
-c = float(input(9))  
+# Integer variable
+a = 10  
+b = 12  
+c = 9  
   
-# calculate the semi-perimeter  
-s = (a + b + c) / 2  
+# Calculate the semi-perimeter
+s = (a + b + c) / 2
   
-# calculate the area  
-area = (s*(s-a)*(s-b)*(s-c)) ** 0.5  
-print('The area of the triangle is %0.2f' %area)   
+# Calculate the area
+area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+
+print('The area of the triangle is', area)
