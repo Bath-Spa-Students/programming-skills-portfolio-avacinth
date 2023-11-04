b93dc1f088f36a9f6475d869e82e86f6dc03f4f7
avacinth <144213493+avacinth@users.noreply.github.com>
2023-11-04 20:32:29 +0400

    exercise 4.py

---
+++ Chapter 4- Control Flow/Exercises/exercise 4.py
@@ -0,0 +1,18 @@
+# Input from the user
+
+age = (input("Input age:"))
+
+# If-elif-else statement
+
+if age <2:
+    print ('The person is a baby')
+elif age <4:
+    print ('The person is a toodler')
+elif age <13:
+    print ('The person is a kid')
+elif age <20:
+    print ('The person is a teenager')
+elif age <65:
+    print ('The person is an adult')
+else:
+    print ('The person is an elder')
