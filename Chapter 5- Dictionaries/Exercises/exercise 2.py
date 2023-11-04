b559b7442683b2237a80dc068e5f5991e612d69d
avacinth <144213493+avacinth@users.noreply.github.com>
2023-11-04 21:31:10 +0400

    exercise 2.py

---
+++ Chapter 5- Dictionaries/Exercises/exercise 2.py
@@ -0,0 +1,26 @@
+# Dictionaries
+
+glossary = {
+    'String': 'A data type that presents a sequence of characters in quotes.',
+    'Integer': 'used to represent whole numbers, both positive and negative.',
+    'Float': 'Used to represent numbers with decimal points.',
+    'Boolean': 'Can have one of two values: True or False.',
+    'List': 'Stores a collection of items in an ordered sequence.',
+    }
+
+# String concatenation
+
+word = 'String'
+print("\n" + word + ": " + glossary[word])
+
+word = 'Integer'
+print("\n" + word + ": " + glossary[word])
+
+word = 'Float'
+print("\n" + word + ": " + glossary[word])
+
+word = 'Boolean'
+print("\n" + word + ": " + glossary[word])
+
+word = 'List'
+print("\n" + word + ": " + glossary[word])
