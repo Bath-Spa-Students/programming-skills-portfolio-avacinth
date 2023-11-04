ce8b33a8f91707b793f65d795d0c85dfcdde016a
avacinth <144213493+avacinth@users.noreply.github.com>
2023-11-04 23:54:00 +0400

    exercise 5.py

--- Chapter 1- Getting Started with Python/Exercises/exercise 5.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise 5.py
@@ -1,16 +1,14 @@
-#Exercise 5: Compute area of Circle 
+# Computing the area of a circle 
 
-#Python Program to fine the area of triangle
-a=4
-b=5
-c=6
-# a= float(input('Enter first side:: '))
-# b= float(input('Enter second side:: '))
-# c= float(input('Enter third side:: '))
+# Integer variable
+a = 4
+b = 5
+c = 6
 
-#calculate the semi-perimeter
+# Calculate the semi-perimeter
 s=(a+b+c)/2
 
-#calculate the area
-area=(s*(s-a)*(s-b)*(s-c))**0.5
-print('The are of the triangle is %0.2f'%area)
+# Calculate the area
+area = (s*(s-a)*(s-b)*(s-c))**0.5
+
+print('The area of the triangle is', area)
