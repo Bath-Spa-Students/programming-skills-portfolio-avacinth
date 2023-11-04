224f7bc49377cfc2766b5e0ed8f4ed487a3b601b
avacinth <144213493+avacinth@users.noreply.github.com>
2023-11-04 21:30:34 +0400

    exercise 1.py

---
+++ Chapter 5- Dictionaries/Exercises/exercise 1.py
@@ -0,0 +1,13 @@
+# Dictionaries
+
+person = {'first_name' : 'Ivory' , 
+           'last_name' : 'Bau' ,
+           'age' : '19' ,
+           'city' : 'Pampanga'}
+
+# Returning values
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
