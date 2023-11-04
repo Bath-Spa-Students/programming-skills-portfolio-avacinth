248098d3559d03125a8ab7361369c264dffdcc07
avacinth <144213493+avacinth@users.noreply.github.com>
2023-11-04 20:23:36 +0400

    exercise 2_2nd version_else block.py

---
+++ Chapter 4- Control Flow/Exercises/exercise 2_2nd version_else block.py
@@ -0,0 +1,10 @@
+# String variable
+
+alien_color = 'red'
+
+# If-else statement
+
+if alien_color == 'green':
+    print ("Congratulations! You just earned 5 points.")
+else:
+    print ("Congratulations! You just earned 10 points.")
