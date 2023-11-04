847cbaa7343cbb0aa2e6583529995c15914710f7
avacinth <144213493+avacinth@users.noreply.github.com>
2023-11-04 20:25:18 +0400

    exercise 3_2nd version.py

---
+++ Chapter 4- Control Flow/Exercises/exercise 3_2nd version.py
@@ -0,0 +1,12 @@
+# String variable
+
+alien_color = 'yellow'
+
+# If-elif-else statement
+
+if alien_color == 'green':
+    print ("Congratulations! You just earned 5 points.")
+elif alien_color == 'yellow':
+    print("You just earned 10 points!")
+else:
+    print ("Congratulations! You just earned 15 points.")
