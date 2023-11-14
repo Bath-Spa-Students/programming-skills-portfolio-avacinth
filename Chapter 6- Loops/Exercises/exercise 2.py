e9c98600bcd49685b28d1619be8bd1579359447b
avacinth <144213493+avacinth@users.noreply.github.com>
2023-11-14 23:44:54 +0400

    Create exercise 2.py

---
+++ Chapter 6- Loops/Exercises/exercise 2.py
@@ -0,0 +1,10 @@
+# user input
+age = input("How old are you? ")
+
+#if-elif-else statement
+if age < 3: 
+    print (" You get in free!")
+elif age < 13:
+    print (" Your ticket is $10.")
+else:
+    print (" Your ticket is $15.")
