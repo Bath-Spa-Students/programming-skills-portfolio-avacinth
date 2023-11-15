0e9883061eb0fb9e8132cc704208a49a77e81368
avacinth <144213493+avacinth@users.noreply.github.com>
2023-11-15 19:33:03 +0400

    exercise 3.py

---
+++ Chapter 7- Functions/Exercises/exercise 3.py
@@ -0,0 +1,6 @@
+def make_shirt (size, message):
+    print("Making a shirt of size " + size + " with the message: '" + message + "'.")
+
+make_shirt("Small", "Hello World!")
+
+make_shirt(size = "Medium", message = "It's me, hi")
