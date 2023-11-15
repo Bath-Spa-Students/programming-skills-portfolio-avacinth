f6f4d100db55417b0919de4e8dda1d9952ec729b
avacinth <144213493+avacinth@users.noreply.github.com>
2023-11-15 18:31:22 +0400

    exercise 1.py

--- Chapter 7- Functions/Exercises/exercise 1.py
+++ Chapter 7- Functions/Exercises/exercise 1.py
@@ -1,4 +1,4 @@
 def display_message():
-    print ("I learned in this chapter the use of functions in where it can only run when it's called.")
+    print ("This chapter taught me how to use functions that run only when they are called.")
 
 display_message()
