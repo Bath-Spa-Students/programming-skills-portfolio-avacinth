479a17a78287a91db1504a457b70041477be5b92
avacinth <144213493+avacinth@users.noreply.github.com>
2023-11-16 20:03:56 +0400

    practice 5.py

---
+++ Chapter 2- Variables & Comments/Exercises/practice 5.py
@@ -0,0 +1,9 @@
+# Create a list
+fruits = ["apple", "orange", "banana", "blueberry", "strawberry"]
+
+# List index
+a, d, g = fruits[0], fruits[1], fruits[2]
+
+print("a:", a)
+print("d:", d)
+print("g:", g)
