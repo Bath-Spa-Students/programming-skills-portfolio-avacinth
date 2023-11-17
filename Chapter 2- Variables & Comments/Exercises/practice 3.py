0cf97e26111c99b3a1c94e0203faf0fab6e17677
avacinth <144213493+avacinth@users.noreply.github.com>
2023-11-17 14:06:12 +0400

    practice 3.py

---
+++ Chapter 2- Variables & Comments/Exercises/practice 3.py
@@ -0,0 +1,11 @@
+# Input from user
+num = input("Enter a number: ")
+
+# Type cast / converting
+str = str(num)
+int = int(num)
+float = float(num)
+
+print("As string:", str)
+print("As integer:", int)
+print("As float:", float)
